c3dc3e4f812ee54047e2cddb195869b28f72bf6b
rsm2-sguddeti <saket.guddeti@gmail.com>
2018-01-17 02:19:49 -0800

    final raw code

--- code.py
+++ code.py
@@ -7,14 +7,13 @@ os.getcwd()
 os.chdir('/Users/saketguddeti/Desktop/git/walmart-trip-type')
 np.set_printoptions(threshold = np.nan)
 
-####################################################################################
+
 ####################################################################################
 ############################# Importing the data ###################################
 ####################################################################################
-####################################################################################
-data = pd.read_csv('Data/train.csv')
-data.head()
-data.shape
+
+train_data = pd.read_csv('Data/train.csv')
+test_data = pd.read_csv('Data/test.csv')
 
 ####################################################################################
 ####################################################################################
@@ -40,62 +39,102 @@ test = data
 test['leng'] = test['Upc'].apply(lambda x: len(str(x)))
 set(test.leng)
 '''
-####################################################################################
+
 ####################################################################################
 ############################# Cleaning the dataset #################################
 ####################################################################################
-####################################################################################
 
-data = data.dropna(how = 'any') # Removing Null Value rows
+# Concatenating train and test data
+train_data["type"] = "Train"
+train_data = train_data.loc[pd.notnull(train_data['Upc']),:] 
+test_data["type"] = "Test"
+test_data.insert(0, column = 'TripType', value = [-1234 for i in range(test_data.shape[0])])
+raw_data = pd.concat([train_data,test_data], axis = 0)
+
+# Finding the most frequent UPC-Fineline Number for departments
+most_freq_dept_upc_fln = raw_data.groupby(['DepartmentDescription','FinelineNumber','Upc']).size().reset_index()
+most_freq_dept_upc_fln = most_freq_dept_upc_fln.sort_values(['DepartmentDescription',0], ascending = False)
+most_freq_dept_upc_fln = most_freq_dept_upc_fln.groupby(['DepartmentDescription']).first().reset_index()
+most_freq_dept_upc_fln.columns = ['DepartmentDescription','fln','up',0]
+
+# Filling out missing UPC/FineLineNumber values using DepartmentDescription
+data = pd.merge(left=raw_data, right=most_freq_dept_upc_fln,how='left',
+                left_on='DepartmentDescription',right_on='DepartmentDescription')
+
+#test = data[data.isnull().any(axis = 1)]
+data['Upc'] = np.where((pd.isnull(data['Upc'])) & (pd.notnull(data['DepartmentDescription'])),
+    data['up'], data['Upc'])
+data['FinelineNumber'] = np.where((pd.isnull(data['FinelineNumber'])) & (pd.notnull(data['DepartmentDescription'])),
+    data['fln'], data['FinelineNumber'])
+data = data.drop(['up','fln',0],axis = 1)
+
+# Filling out missing UPC/FinelIne/Department by the most popular ones wrt weekday
+most_freq_week_dept = raw_data.groupby(['Weekday','DepartmentDescription','FinelineNumber','Upc']).size().reset_index()
+most_freq_week_dept = most_freq_week_dept.sort_values(['Weekday',0], ascending = False)
+most_freq_week_dept = most_freq_week_dept.groupby('Weekday').first().reset_index()
+most_freq_week_dept.columns = ['Weekday','DeptDesc','fln','up', 0]
+
+
+data = pd.merge(left=data, right=most_freq_week_dept, how='left',
+                left_on = 'Weekday', right_on='Weekday')
+data['DepartmentDescription'] = np.where(pd.isnull(data['DepartmentDescription']), 
+         data['DeptDesc'], data['DepartmentDescription'])
+data['Upc'] = np.where(pd.isnull(data['Upc']), 
+         data['up'], data['Upc'])
+data['FinelineNumber'] = np.where(pd.isnull(data['FinelineNumber']), 
+         data['fln'], data['FinelineNumber'])
+data = data.drop(['DeptDesc','up','fln',0],axis = 1)
+
+
+# data = data.dropna(how = 'any') # Removing Null Value rows
 data = data.loc[data['ScanCount'] > -1,:] # Removing rows for product return
+data2 = data
+ 
+# Aggregating number of items bought with respect to visit number and UPC
+upc_sum = data2.groupby(['VisitNumber','Upc'])['ScanCount'].sum()
+upc_sum = upc_sum.reset_index()
+data2 = data2.drop('ScanCount',axis = 1)
+data2 = pd.merge(left=data2, right=upc_sum, how='left', left_on=['VisitNumber','Upc'],right_on=['VisitNumber','Upc'])
+data2 = data2.drop_duplicates() # Removing duplicates in the data
 
-
-# Aggregating number if items bought with respect to visit id and upc
-data2 = data.groupby(['VisitNumber','Upc'])['ScanCount'].sum()
-data2 = data2.reset_index()
-data = data.drop('ScanCount',axis = 1)
-data = pd.merge(left=data, right=data2, how='left', left_on=['VisitNumber','Upc'],right_on=['VisitNumber','Upc'])
-data = data.drop_duplicates() # Removing duplicates in the data
-
-unique_items = data.groupby(['VisitNumber', 'Upc']).size()
-unique_items = unique_items.reset_index()
+# Aggregating number of unique items purchased per visit
+unique_items = data2.groupby(['VisitNumber', 'Upc']).size().reset_index()
 unique_items.columns = ('VisitNumber', 'Upc' , 'UniqueItems')
 unique_items = unique_items.groupby('VisitNumber')['UniqueItems'].sum().reset_index()
-total_items = data.groupby('VisitNumber')['ScanCount'].sum().reset_index()
+total_items = data2.groupby('VisitNumber')['ScanCount'].sum().reset_index()
 
-
-unique_depts = data.groupby(['VisitNumber', 'DepartmentDescription']).size().reset_index()
+# Aggregating number of unique department items purchased per visit
+unique_depts = data2.groupby(['VisitNumber', 'DepartmentDescription']).size().reset_index()
 unique_depts = unique_depts.groupby(['VisitNumber']).size().reset_index()
 unique_depts.columns = ('VisitNumber', 'UniqueDepts')
 
-
-unique_models = data.groupby(['VisitNumber', 'FinelineNumber']).size().reset_index()
+# Aggregating number of unique model items purchased per visit
+unique_models = data2.groupby(['VisitNumber', 'FinelineNumber']).size().reset_index()
 unique_models = unique_models.groupby(['VisitNumber']).size().reset_index()
 unique_models.columns = ('VisitNumber', 'UniqueModels')
 
-data = pd.merge(left=data, right=unique_items, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
-data = data.drop('ScanCount',axis=1)
-data = pd.merge(left=data, right=total_items, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
-data = pd.merge(left=data, right=unique_depts, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
-data = pd.merge(left=data, right=unique_models, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
+# Merging all the features into the dataframe
+data2 = pd.merge(left=data2, right=unique_items, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
+data2 = data2.drop('ScanCount',axis=1)
+data2 = pd.merge(left=data2, right=total_items, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
+data2 = pd.merge(left=data2, right=unique_depts, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
+data2 = pd.merge(left=data2, right=unique_models, how = 'left', left_on ='VisitNumber',right_on='VisitNumber')
 
-data2 = data
 
-###########TFIDF################
+# Feature for reflecting the rarity of the item purchased and hence the department
 idf_score = data2.groupby('Upc')['VisitNumber'].apply(list).reset_index()
 total_visits = len(set(data2.VisitNumber))
 idf_score['freq'] = idf_score['VisitNumber'].apply(lambda x: np.log(total_visits/len(x)))
 
 idf_dic = {idf_score['Upc'][i]:idf_score['freq'][i] for i in range(idf_score.shape[0])}
 
-dept_score = data[['DepartmentDescription','Upc']].drop_duplicates()
+dept_score = data2[['DepartmentDescription','Upc']].drop_duplicates()
 dept_score['dept_idf'] = dept_score['Upc'].map(idf_dic)
 dept_score = dept_score.groupby('DepartmentDescription')['dept_idf'].agg('mean').reset_index()
 dept_score = dept_score.set_index('DepartmentDescription').to_dict()['dept_idf']
 data2['idf_score'] = data2['DepartmentDescription'].map(dept_score)
 
-##################################
-
+# Aggregating Department, Fineline Number and IDF Score wrt Visit Number
 data2['FinelineNumber'] = data2['FinelineNumber'].apply(lambda x: str(x))
 
 depts = data2.groupby('VisitNumber')['DepartmentDescription'].apply(list).reset_index()
@@ -107,7 +146,7 @@ data2 = pd.merge(left=data2,right=depts,how='left',left_on='VisitNumber',right_o
 data2 = pd.merge(left=data2,right=fln,how='left',left_on='VisitNumber',right_on='VisitNumber')
 data2 = pd.merge(left=data2,right=idf,how='left',left_on='VisitNumber',right_on='VisitNumber')
 
-####################################
+# Feature for TF-IDF scores for the department description
 import nltk
 from nltk.corpus import stopwords
 import re
@@ -138,10 +177,10 @@ for lt in list(depts['DepartmentDescription']):
 from sklearn.feature_extraction.text import TfidfVectorizer
 vect = TfidfVectorizer(ngram_range=(1,1), min_df=0, max_features = 1000)
 vect1 = TfidfVectorizer(ngram_range=(2,9), min_df=0, analyzer = 'char', max_features = 2000)
-train_desp_vec = vect.fit_transform(word_list)
-train_desp_vec1 = vect1.fit_transform(word_list)
+vec = vect.fit_transform(word_list)
+vec1 = vect1.fit_transform(word_list)
 
-train_tfidf = np.hstack([train_desp_vec.toarray() , train_desp_vec1.toarray()])
+train_tfidf = np.hstack([vec.toarray() , vec1.toarray()])
 from sklearn.decomposition import PCA
 pca = PCA(n_components = 1000)
 tfidf_data = pca.fit_transform(train_tfidf)
@@ -149,26 +188,15 @@ tfidf_data = pd.DataFrame(tfidf_data)
 data2 = pd.concat([data2, tfidf_data], axis=1)
 
 ####################################################################################
-####################################################################################
-############################# Scaling the data #####################################
-####################################################################################
+############################# Encoding Categorical Data ############################
 ####################################################################################
 
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-data2[['UniqueItems','ScanCount','UniqueDepts','UniqueModels','idf_score']] = sc_X.fit_transform(data2[['UniqueItems','ScanCount','UniqueDepts','UniqueModels','idf_score']])
-
-####################################################################################
-####################################################################################
-############################# Encoding Categorical Data #####################################
-####################################################################################
-####################################################################################
-
-# Encoding Categorical Data
+# Encoding Weekday values
 x = pd.get_dummies(data2['Weekday'], drop_first = True)
 data2 = pd.concat([data2,x], axis = 1)
 data2 = data2.drop('Weekday', axis = 1)
 
+# Encoding Department values
 from sklearn.preprocessing import MultiLabelBinarizer
 multilabelbinarizer = MultiLabelBinarizer()
 x = pd.DataFrame(multilabelbinarizer.fit_transform(data2['DepartmentDescription']), 
@@ -176,7 +204,7 @@ x = pd.DataFrame(multilabelbinarizer.fit_transform(data2['DepartmentDescription'
 data2 = pd.concat([data2,x], axis = 1)
 data2 = data2.drop('DepartmentDescription', axis = 1)
 
-
+# Encoding Fineline Number values
 x = pd.DataFrame(multilabelbinarizer.fit_transform(data2['FinelineNumber']), 
                  columns = multilabelbinarizer.classes_)
 data2 = data2.drop('FinelineNumber', axis = 1)
@@ -187,8 +215,7 @@ pca_fln = pca.fit_transform(x)
 pca_fln = pd.DataFrame(pca_fln, columns=["fln"+str(x) for x in range(100)])
 data2 = pd.concat([data2,pca_fln], axis = 1)
 
-######################################################
-
+# Capturing interaction between highly associated departments
 dept_corr = data.groupby(['VisitNumber','DepartmentDescription']).size().reset_index()
 dept_corr = pd.merge(left=dept_corr,right=dept_corr,how='left',on='VisitNumber')
 dept_corr = dept_corr.groupby(['DepartmentDescription_x','DepartmentDescription_y']).size().reset_index()
@@ -217,7 +244,6 @@ dept_diff = pd.DataFrame(dept_diff, columns = ["dept"+str(x) for x in range(len(
 dept_diff['VisitNumber'] = dept_freq_ind
 
 data2 = pd.merge(left=data2,right=dept_diff,how='left',left_on='VisitNumber',right_on='VisitNumber')
-data2.iloc[:,-68:] = sc_X.fit_transform(data2.iloc[:,-68:])
 
 '''
 import networkx as nx    
@@ -227,10 +253,27 @@ nx.draw(G, pos=nx.spring_layout(G), with_labels=True, node_size = 4, width = 0.2
 plt.savefig("labels.png", format="PNG",dpi=1000)
 '''
 
-####################################################
+####################################################################################
+############################# Scaling the data #####################################
+####################################################################################
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+data2[['UniqueItems','ScanCount','UniqueDepts','UniqueModels','idf_score']] = sc_X.fit_transform(data2[['UniqueItems','ScanCount','UniqueDepts','UniqueModels','idf_score']])
+data2.iloc[:,-68:] = sc_X.fit_transform(data2.iloc[:,-68:])
+
+# Splitting the dataframe into train/test data
+train = data2.loc[data2['type']=="Train",:]
+X_train = train.iloc[:,3:]
+y_train = train.iloc[:,0]
 
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(data2.iloc[:,2:] ,data2.iloc[:,0],test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split( train.iloc[:,3:] ,train.iloc[:,0],test_size = 0.2)
+
+
+test = data2.loc[data2['type']=="Test",:]
+X_test = test.iloc[:,3:]
+y_test = test.iloc[:,0]
+
 
 # Applying Random Forest Classifier
 '''
@@ -254,17 +297,9 @@ y_pred = classifier.predict(X_test)
 sum(y_pred == y_test)/18041
 '''
 
-
-
-
-
-
-
-
-
 import xgboost as xgb
 
-label_dict = pd.DataFrame(list(set(data.TripType)))
+label_dict = pd.DataFrame(list(set(train.TripType)))
 label_dict['ind'] = pd.Series(list(label_dict.index))
 label_dict = label_dict.set_index(0).to_dict()['ind']
 y_train = y_train.map(label_dict)
@@ -275,16 +310,16 @@ xg_test = xgb.DMatrix(X_test.values, label=y_test.values)
 # setup parameters for xgboost
 param = {}
 # use softmax multi-class classification
-param['objective'] = 'multi:softmax'
+param['objective'] = 'multi:softprob'
 # scale weight of positive examples
-param['eta'] = 0.2
+param['eta'] = 0.1
 param['max_depth'] = 6
 param['silent'] = 1
 param['nthread'] = 4
 param['num_class'] = 38
 
 watchlist = [(xg_train, 'train'), (xg_test, 'test')]
-num_round = 15
+num_round = 5
 bst = xgb.train(param, xg_train, num_round, watchlist)
 # get prediction
 y_pred = bst.predict(xg_test)
